5d59ba497475a0dcd6be6345c6f2e7a19ce61213
unknown <loreta.padaigaite@codeacademylt.onmicrosoft.com>
2023-01-05 18:22:49 +0200

    text-overflow

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -13,26 +13,23 @@ const ExpensesListItem = styled.li `
     border-radius: 10px;
     box-shadow: 0 5px 7px -1px rgb(51 51 51 / 23%); 
     display: flex;
+    align-items: center;
     justify-content: space-between;
     padding: 10px 30px;    
 `;
 
 const ExpensesAmount = styled.span`
-    align-items: center;
     color: #35d8ac;
-    display: flex;
     font-size: 34px;
     font-weight: 700;
 `;
 const ExpensesType = styled.span`
-    align-items: center;
     color: #979cb0;
-    display: flex;
     font-size: 20px
     font-weight: 600;
     text-overflow: ellipsis;
     white-space: nowrap;
-    overflow: auto;
+    overflow: hidden;
 `;
 
 
